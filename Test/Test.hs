077acfc79e33c53b4e89d87f95a86d0b703e2810
Matthías Páll Gissurarson <mpg@mpg.is>
2020-06-12 12:09:00 +0200

    Show that importing doesn't help

--- Test/Test.hs
+++ Test/Test.hs
@@ -2,9 +2,18 @@
 {-# LANGUAGE TypeFamilies #-}
 module Main where
 
+-- Even if we import the TyCon being used  and use it in the code,
+--  we still get the same error
+-- import Plugin (PluginTyCon(..))
+
+-- fP :: () ~ Bool => PluginTyCon -> IO ()
+-- fP PTC{} = print "bug"
+-- main :: IO ()
+-- main = f (PTC undefined)
+
 -- We need an unsolveable constraint to trigger the plugin
 f :: () ~ Bool => IO ()
 f = print "bug"
 
 main :: IO ()
-main = f
+main = f 
